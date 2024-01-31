e6db7484ac48945612fc3a0fe9d9f477caa39f4c
Fawzi Gharib <fawz11gharib@gmail.com>
2024-01-31 12:13:03 +0300

    Package Name reinit

--- android/app/src/main/kotlin/com/example/sixvalley_vendor_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sixvalley_vendor_app/MainActivity.kt
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-package com.swift.wavemall.seller
-=======
 package com.wavemall.seller
->>>>>>> 505bf44c219dfa4974f59a80090d4add1b93607a
 
 import io.flutter.embedding.android.FlutterActivity
 
